3b0ef234ad81a1ecf9ed8af48a0fb6e0e53bbb61
subin9292 <152476106+subin9292@users.noreply.github.com>
2024-06-15 18:59:34 +0900

    Delete HowsTheWeather directory

--- HowsTheWeather/app/init.py
+++
@@ -1,1 +0,0 @@
-# appinit__.py
--- HowsTheWeather/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Comment(Base):
-    __tablename__ = "comments"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    comment = Column(String, index=True)
